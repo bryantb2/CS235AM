13cd2532429225c9d4951a23a8f902a17ddc2496
Blake <bryantb2@my.lanecc.edu>
2019-10-02 21:21:27 -0700

    finished writing methods for main activity class and percentage class

--- app/src/main/java/com/example/tipcalculator/MainActivity.java
+++ app/src/main/java/com/example/tipcalculator/MainActivity.java
@@ -3,13 +3,76 @@ package com.example.tipcalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.tipcalculator.Percentage;
 
 public class MainActivity extends AppCompatActivity {
+
+    //class fields
+    Button incrementorButton;
+    Button decrementorButton;
+    EditText subTotalEntry;
+    TextView tipPercentOutput;
+    TextView tipTotalOutput;
+    TextView billTotalOutput;
+
+    Percentage tipPercent = new Percentage();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //FETCHING UI ELEMENTS
+        this.incrementorButton = findViewById(R.id.raisePercentButton);
+        this.decrementorButton = findViewById(R.id.lowerPercentButton);
+        this.subTotalEntry = findViewById(R.id.subTotalTextInput);
+        this.tipPercentOutput = findViewById(R.id.tipPercentOutputTag);
+        this.tipTotalOutput = findViewById(R.id.tipTotalOutputTag);
+        this.billTotalOutput = findViewById(R.id.billTotalOutputTag);
+    }
+
+    public void percentButtonEventHanlder(String operation) {
+        //check and validate the operation
+        if (operation == "increment" || operation == "decrement") {
+            if(operation == "increment") {
+                //increment tip
+                tipPercent.incrementPercent();
+            }
+            else {
+                //decrement tip
+                tipPercent.decrementPercent();
+            }
+
+            //get subTotal from UI
+            double subTotal = Double.parseDouble(this.subTotalEntry.getText().toString());
+            //run calculation
+            double tipTotal = this.calculateTipTotal(tipPercent.getPercent(),subTotal);
+            //final bill total
+            double billTotal = tipTotal + subTotal;
+
+            //get FINAL values for output
+            String finalTipPercent = String.valueOf(tipPercent.getPercent());
+            String finalTipTotal = String.valueOf(tipTotal);
+            String finalBillTotal = String.valueOf(billTotal);
+
+            //DISPLAY ON UI
+            tipPercentOutput.setText(finalTipPercent + "%");
+            tipTotalOutput.setText(finalTipTotal);
+            billTotalOutput.setText(finalBillTotal);
+        }
+        else {
+            throw new java.lang.IllegalArgumentException("argument must be string and either 'increment' or 'decrement");
+        }
+
+    }
+
+    private double calculateTipTotal(int tipPercent, double billSubTotal) {
+        double tipAsDecimal = (tipPercent/100D);
+        return (billSubTotal * tipAsDecimal);
     }
 }
 
--- app/src/main/java/com/example/tipcalculator/Percentage.java
+++ app/src/main/java/com/example/tipcalculator/Percentage.java
@@ -2,18 +2,39 @@ package com.example.tipcalculator;
 
 public class Percentage {
     //class fields
-    int tipPercent;
+    private int tipPercent;
 
     //Constructor
-    public Percentage(int percent = 15) {
-        this.tipPercent = percent;
+    public Percentage(int userSpecifiedPercent) {
+        if(userSpecifiedPercent >= 1) {
+            if (userSpecifiedPercent <100) {
+                this.tipPercent = percent;
+            }
+            else {
+                throw new java.lang.IllegalArgumentException("tip percent must be less than 100");
+            }
+        }
+        else {
+            throw new java.lang.IllegalArgumentException("tip percent must be greater than 0");
+        }
+    }
+
+    public Percentage() {
+        this.tipPercent = 15;
     }
 
     //Properties
-    public int Percent {
-        get
-        {
+    public int getPercent() {
+        return this.tipPercent;
+    }
 
+    public void incrementPercent() {
+        this.tipPercent = this.tipPercent++;
+    }
+
+    public void decrementPercent() {
+        if (this.tipPercent > 0) {
+            this.tipPercent = this.tipPercent--;
         }
     }
 }
