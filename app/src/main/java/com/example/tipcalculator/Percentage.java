b5b2a3e456419056b38925c921c55b07c605b92e
bryantb2 <bryantb2@my.lanecc.edu>
2019-10-02 19:45:12 -0700

    adding classes

---
+++ app/src/main/java/com/example/tipcalculator/Percentage.java
@@ -0,0 +1,19 @@
+package com.example.tipcalculator;
+
+public class Percentage {
+    //class fields
+    int tipPercent;
+
+    //Constructor
+    public Percentage(int percent = 15) {
+        this.tipPercent = percent;
+    }
+
+    //Properties
+    public int Percent {
+        get
+        {
+
+        }
+    }
+}
