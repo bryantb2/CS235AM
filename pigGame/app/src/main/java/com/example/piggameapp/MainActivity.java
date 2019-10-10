d0b207e1aad8bd34c33f52204c52e80ac3a0dcb0
Blake <bryantb2@my.lanecc.edu>
2019-10-10 00:47:46 -0700

    adding UI methods for updating labels and validating user input

--- pigGame/app/src/main/java/com/example/piggameapp/MainActivity.java
+++ pigGame/app/src/main/java/com/example/piggameapp/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.piggameapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.renderscript.Sampler;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -52,6 +54,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void NewGameButtonClick() {
+        //get the text from both edit text fields
+        //if data in fields is valid, create PigGame object
 
     }
 
@@ -60,7 +64,39 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void UpdatePlayerScore
+    private void UpdatePlayerScore(int playerNumber, int score) {
+        if(AreUsernamesValid() != false) {
+            if(playerNumber==1) {
+                this.player1ScoreLabel.setText(String.valueOf(score));
+            }
+            else {
+                this.player2ScoreLabel.setText(String.valueOf(score));
+            }
+        }
+    }
+
+    private void UpdateCurrentPlayer(String currentPlayerUsername) {
+        this.currentPlayerLabel.setText(currentPlayerUsername);
+    }
+
+    private void UpdatePointsRunningTotal(int points) {
+        this.pointsAccumulatorLabel.setText(String.valueOf(points));
+    }
+
+    private boolean AreUsernamesValid() {
+        String player1Username = this.player1Username.getText().toString();
+        String player2Username = this.player2Username.getText().toString();
+        //if empty
+        if(!(player1Username.length() != 0)) {
+            Toast.makeText(this,"Player 1 username cannot be empty!",Toast.LENGTH_LONG);
+            return false;
+        }
+        if(!(player2Username.length() != 0)) {
+            Toast.makeText(this,"Player 2 username cannot be empty!",Toast.LENGTH_LONG);
+            return false;
+        }
+        return true;
+    }
 
 
 
