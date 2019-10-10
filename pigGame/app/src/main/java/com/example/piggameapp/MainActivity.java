6c6147406eb4563f86a2bb3a69ee20564bef53d6
Blake <bryantb2@my.lanecc.edu>
2019-10-10 00:20:07 -0700

    updated UI, working on UI connectivity and main activity class; finished pig class

--- pigGame/app/src/main/java/com/example/piggameapp/MainActivity.java
+++ pigGame/app/src/main/java/com/example/piggameapp/MainActivity.java
@@ -3,27 +3,64 @@ package com.example.piggameapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     //CLASS FIELDS
-    private PigGame pigGame = new PigGame();
+    private PigGame pigGame;
+    private EditText player1Username;
+    private EditText player2Username;
+    private TextView player1ScoreLabel;
+    private TextView player2ScoreLabel;
+    private ImageView dieImage;
+    private TextView currentPlayerLabel;
+    private TextView pointsAccumulatorLabel;
+    private Button rollDieButton;
+    private Button endTurnButton;
+    private Button newGameButton;
 
     //LIFECYCLES
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //GETTING UI ELEMENTS
+        this.player1Username = findViewById(R.id.player1Username_TextEntry);
+        this.player1ScoreLabel = findViewById(R.id.player1Score_Label);
+        this.player2ScoreLabel = findViewById(R.id.player2Score_Label);
+        this.player2Username = findViewById(R.id.player2Username_TextEntry);
+        this.currentPlayerLabel = findViewById(R.id.currentPlayer_Label);
+        this.dieImage = findViewById(R.id.dieRollResult_Label);
+        this.pointsAccumulatorLabel = findViewById(R.id.runningPointsTotal_Label);
+        this.rollDieButton = findViewById(R.id.rollDie_Button);
+        this.endTurnButton = findViewById(R.id.endTurn_Button);
+        this.newGameButton = findViewById(R.id.newGame_Button);
     }
 
     //EVENT HANDLERS
-    public void rollClick() {
+    public void RollButtonClick() {
 
     }
 
+    public void EndTurnButtonClick() {
+
+    }
+
+    public void NewGameButtonClick() {
+
+    }
 
     //METHODS
+    public void UpdateDieImage(int rolledNumber) {
 
+    }
+
+    public void UpdatePlayerScore
 
 
 
