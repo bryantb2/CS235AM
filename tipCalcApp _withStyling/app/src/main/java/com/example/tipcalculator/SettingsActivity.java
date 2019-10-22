070a7aba42fc7ca964dfc9e92bcde269234876a0
bryantb2 <bryantb2@my.lanecc.edu>
2019-10-22 16:40:16 -0700

    added fragment and activity classes for options dropdown

--- tipCalcApp _withStyling/app/src/main/java/com/example/tipcalculator/SettingsActivity.java
+++ tipCalcApp _withStyling/app/src/main/java/com/example/tipcalculator/SettingsActivity.java
@@ -1,4 +1,22 @@
 package com.example.tipcalculator;
 
-public class SettingsActivity {
+import android.os.Bundle;
+import android.app.Activity;
+
+public class SettingsActivity extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //show fragment class as main content
+        /*
+        adds fragment to activity by
+            1. opening up a transaction between the fragment and activity
+            2. replacing the content in the activity with the fragment object
+            3. commiting those changes to the new activity
+         */
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content,new SettingsActivityFragment())
+                .commit();
+    }
 }
---
+++ tipCalcApp _withStyling/app/src/main/java/com/example/tipcalculator/SettingsActivityFragment.java
@@ -0,0 +1,14 @@
+package com.example.tipcalculator;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+public class SettingsActivityFragment extends PreferenceFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //loads preferences from the XML files
+        addPreferencesFromResource(R.xml.preferences);
+    }
+}
