2d28fded2351475ddb4fe05b3f77b0dbeabbb4de
Blake <bryantb2@my.lanecc.edu>
2019-10-24 21:27:16 -0700

    removed TODO from preferences.xml (file is finished); max points setting fully functional!

--- pigGamePart2/app/src/main/java/com/example/piggameapp/MainActivity.java
+++ pigGamePart2/app/src/main/java/com/example/piggameapp/MainActivity.java
@@ -122,7 +122,7 @@ public class MainActivity extends AppCompatActivity {
     //TODO: create instance variables at the class level to get and set user's custom settings from the defaultPreferences object       ---DONE---
     //TODO: add AI rolling logic (calc, roll, display, ect.)        --- DONE ----
     //TODO: add functionality for two dice
-    //TODO: modify game logic to accept a changing max game score
+    //TODO: modify game logic to accept a changing max game score         --- DONE ----
     //TODO: wire the entire app to dynamically change logic, using bools set at the class level, and display methods based off the user's desired game settings
 
     @Override
