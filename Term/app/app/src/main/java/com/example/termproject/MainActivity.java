f3355d5b70cc9500d1ae0502482801268f7be65e
Blake <bryantb2@my.lanecc.edu>
2019-11-26 23:37:09 -0800

    added labels and spinners to main xml layout for quiz

--- Term/app/app/src/main/java/com/example/termproject/MainActivity.java
+++ Term/app/app/src/main/java/com/example/termproject/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.termproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    // WIDGET REFERENCES
+    Button calculateButton;
+    Button resetButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        calculateButton = findViewById(R.id.calculateButton);
+        resetButton = findViewById(R.id.resetButton);
+    }
+
+
+    private void BuildLayoutSpinners() {
+
     }
 }
