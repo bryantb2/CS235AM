51d73f359068ec1f508ccd8ca973ebdc9c110332
Blake <bryantb2@my.lanecc.edu>
2019-11-30 12:30:23 -0800

    added UI Ids to main quiz layout; working on build adapters, for each question, dynamically

--- Term/app/app/src/main/java/com/example/termproject/MainActivity.java
+++ Term/app/app/src/main/java/com/example/termproject/MainActivity.java
@@ -17,10 +17,43 @@ public class MainActivity extends AppCompatActivity {
 
         calculateButton = findViewById(R.id.calculateButton);
         resetButton = findViewById(R.id.resetButton);
+
+        // Dynamically build out array adapters
     }
 
 
     private void BuildLayoutSpinners() {
+        // UI property constants
+        final String spinnerIdDbPrefix = "db";
+        final String spinnerIdBackendPrefix = "backend";
+        final String spinnerIdFrontendPrefix = "frontend";
+        final String spinnerIdMiddle = "backendQuestion";
+
+        // Build adapter from XML array and set to UI element
+        boolean stillDBQuestions = true;
+        boolean stillBackendQuestions = true;
+        boolean stillFrontendQuestions = true;
+        boolean stillGeneralQuestions = true;
+        while(stillGeneralQuestions == true || stillBackendQuestions == true || stillDBQuestions ==true
+                || stillFrontendQuestions == true) {
+            // Set general questions first
+            if(stillGeneralQuestions != false) {
+
+            }
+            // Set frontend questions second
+            else if(stillFrontendQuestions != false) {
+
+            }
+            // Set backend questions third
+            else if(stillBackendQuestions != false) {
+
+            }
+            // Set DB questions fourth
+            else {
+
+            }
+
+        }
 
     }
 }
